1e88cc64addc9b5864ae6feb4a7168daf86a9e46
Karan Hallon <Karan Hallon>
2016-12-02 20:38:47 -0500

    It's coming along relatively well. Should be done by tomorrow (Saturday)

--- videogame.py
+++ videogame.py
@@ -1,6 +1,9 @@
 import pygame
 pygame.init()
 
+from pygame.locals import Rect, DOUBLEBUF, QUIT, K_ESCAPE, KEYDOWN, K_DOWN, \
+    K_LEFT, K_UP, K_RIGHT, KEYUP, K_LCTRL, K_RETURN, FULLSCREEN
+
 white = (255,255,255)
 black = (0,0,0)
 red = (255, 0, 0)
@@ -24,16 +27,59 @@ clock = pygame.time.Clock()
 
 
 Img = pygame.image.load('Blinky.png')
-end = False-
 
-while not end: 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			end = True
-		print (event)
-		
-	pygame.display.update()
-	clock.tick(30) #This is effectively just frame rate. Right now it is at 30 fps
+#Make the image show up]
+def car(x,y):
+	gameDisplay.blit(Img, (x,y))
 	
+	
+
+def game():
+
+	x = display_width * 0.45
+	y = display_height * 0.8	
+		
+	x_change = 0 #This is for changing directions and shit
+	y_change = 0 
+		
+	end = False
+
+	while not end: 
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				end = True\
+			
+			print (event)
+			
+			
+			if event.type == pygame.KEYDOWN:
+				if event.key == K_DOWN:
+					y_change += 5
+				if event.key == K_LEFT:
+					x_change -= 5
+				if event.key == K_RIGHT:
+					x_change += 5
+				if event.key == K_UP:
+					y_change -= 5
+							
+			   
+
+			if event.type == pygame.KEYUP:
+				if event.key == K_LEFT or event.key == K_RIGHT:
+					x_change = 0
+				if event.key == K_UP or event.key== K_DOWN:
+					y_change = 0
+					
+		x = x+ x_change
+		y = y + y_change
+			
+		gameDisplay.fill(white)
+		car(x,y)
+							
+			
+		pygame.display.update()
+		clock.tick(30) #This is effectively just frame rate. Right now it is at 30 fps
+		
 	
 pygame.quit()
